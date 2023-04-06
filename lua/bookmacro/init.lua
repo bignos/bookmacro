a9ef224ecdf5b611cdf1283c58f4ab8408506f5a
Bignose <bignos@gmail.com>
2023-04-06 15:50:25 +0200

    Fix relative path issue on import, and add error(FileNotFound)

--- lua/bookmacro/init.lua
+++ lua/bookmacro/init.lua
@@ -10,7 +10,7 @@ BookMacro = BookMacro or {}
 -- Private
 
 local function triml(str)
-    return string.gsub(str, "^%s*(.-)%s*$", "%1")
+	return string.gsub(str, "^%s*(.-)%s*$", "%1")
 end
 
 local function save_to(path)
@@ -18,34 +18,39 @@ local function save_to(path)
 end
 
 local function save()
-    save_to(save_file)
+	save_to(save_file)
 end
 
 local function load_from(path)
-	if vim.fn.filereadable(path) ~= 0 then
+	local full_path = vim.fn.expand(path)
+	if vim.fn.filereadable(full_path) ~= 0 then
 		BookMacro = vim.fn.json_decode(Path.new(path):read())
-        save()
+		save()
+		return true
+	else
+		vim.api.nvim_err_writeln("File '" .. path .. "' Not found")
+		return false
 	end
 end
 
 local function load()
-    load_from(save_file)
+    return load_from(save_file)
 end
 
 local function get_register_list()
-    local output_register = vim.fn.execute("register")
+	local output_register = vim.fn.execute("register")
 
-    local registers = {}
+	local registers = {}
 
-    for line in string.gmatch(output_register, "[^\r\n]+") do
-        local trimed_str = triml(line)
-        if string.find(trimed_str, "^c ") and string.find(trimed_str, "\"%a") then
-            local clean_entry = string.gsub(trimed_str, [[^.*"(.*)$]], [[%1]])
-            table.insert(registers, clean_entry)
-        end
-    end
+	for line in string.gmatch(output_register, "[^\r\n]+") do
+		local trimed_str = triml(line)
+		if string.find(trimed_str, "^c ") and string.find(trimed_str, '"%a') then
+			local clean_entry = string.gsub(trimed_str, [[^.*"(.*)$]], [[%1]])
+			table.insert(registers, clean_entry)
+		end
+	end
 
-    return registers
+	return registers
 end
 
 local function put_to_register(register, string)
@@ -63,7 +68,7 @@ end
 local function insert_and_save_macro(description, macro_reg)
 	local macro = vim.fn.getreg(macro_reg)
 	insert_macro(description, macro)
-    save()
+	save()
 end
 
 local function get_from_user(prompt, func)
@@ -75,44 +80,44 @@ end
 local function get_file_from_user(prompt, default, completion, func)
 	vim.ui.input({
 		prompt = prompt,
-        default = default,
-        completion = completion,
+		default = default,
+		completion = completion,
 	}, func)
 end
 
 -- Public
 function M.setup()
-    load()
+	load()
 
-    -- Declaration of user commands
-    vim.api.nvim_create_user_command('MacroAdd', function()
-        M.addMacro()
-    end, { desc = "Add a macro to BookMacro" })
+	-- Declaration of user commands
+	vim.api.nvim_create_user_command("MacroAdd", function()
+		M.addMacro()
+	end, { desc = "Add a macro to BookMacro" })
 
-    vim.api.nvim_create_user_command('MacroDel', function()
-        M.removeMacro()
-    end, { desc = "Delete a macro from BookMacro" })
+	vim.api.nvim_create_user_command("MacroDel", function()
+		M.removeMacro()
+	end, { desc = "Delete a macro from BookMacro" })
 
-    vim.api.nvim_create_user_command('MacroSelect', function()
-        M.selectMacro()
-    end, { desc = "Select a macro from BookMacro" })
+	vim.api.nvim_create_user_command("MacroSelect", function()
+		M.selectMacro()
+	end, { desc = "Select a macro from BookMacro" })
 
-    vim.api.nvim_create_user_command('MacroExport', function()
-        M.exportMacro()
-    end, { desc = "Export BookMacro to a JSON file" })
+	vim.api.nvim_create_user_command("MacroExport", function()
+		M.exportMacro()
+	end, { desc = "Export BookMacro to a JSON file" })
 
-    vim.api.nvim_create_user_command('MacroImport', function()
-        M.importMacro()
-    end, { desc = "Replace BookMacro with a JSON file" })
+	vim.api.nvim_create_user_command("MacroImport", function()
+		M.importMacro()
+	end, { desc = "Replace BookMacro with a JSON file" })
 end
 
 function M.addMacro()
-    local register_list = get_register_list()
+	local register_list = get_register_list()
 	vim.ui.select(register_list, {
 		prompt = "Select a Macro",
 	}, function(macro, _)
 		if macro then
-            local register = string.sub(macro,1,1)
+			local register = string.sub(macro, 1, 1)
 			get_from_user("Macro description", function(description)
 				if description then
 					insert_and_save_macro(description, register)
@@ -130,8 +135,8 @@ function M.removeMacro()
 		end,
 	}, function(macro, idx)
 		if macro then
-            table.remove(BookMacro, idx)
-            save()
+			table.remove(BookMacro, idx)
+			save()
 		end
 	end)
 end
@@ -154,23 +159,20 @@ function M.selectMacro()
 end
 
 function M.exportMacro()
-    get_file_from_user("Export BookMacro", "bookmacro.json", "file", function(file)
-        if file then
-            save_to(file)
-            print("Export to " .. file .. " [ DONE ]")
-        end
-    end
-    )
+	get_file_from_user("Export BookMacro", "bookmacro.json", "file", function(file)
+		if file then
+			save_to(file)
+			print("Export to " .. file .. " [ DONE ]")
+		end
+	end)
 end
 
 function M.importMacro()
-    get_file_from_user("Import file to BookMacro", "", "file", function(file)
-        if file then
-            load_from(file)
-            print("Import " .. file .. " [ DONE ]")
-        end
-    end
-    )
+	get_file_from_user("Import file to BookMacro", "", "file", function(file)
+		if file and load_from(file) then
+			print("Import " .. file .. " [ DONE ]")
+		end
+	end)
 end
 
 -- Main
