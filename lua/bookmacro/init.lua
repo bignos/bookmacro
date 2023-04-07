d255ab15b92935ca561b7f49d79aeca296717248
Bignose <bignos@gmail.com>
2023-04-07 10:23:38 +0200

    Fix a regex to filter named register

--- lua/bookmacro/init.lua
+++ lua/bookmacro/init.lua
@@ -44,7 +44,8 @@ local function get_register_list()
 
 	for line in string.gmatch(output_register, "[^\r\n]+") do
 		local trimed_str = triml(line)
-		if string.find(trimed_str, "^c ") and string.find(trimed_str, '"%a') then
+        print("DEBUG: " .. trimed_str)
+		if string.find(trimed_str, '^c  "%a') then
 			local clean_entry = string.gsub(trimed_str, [[^.*"(.*)$]], [[%1]])
 			table.insert(registers, clean_entry)
 		end
